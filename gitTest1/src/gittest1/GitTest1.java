2b8920cd9702c4e853df3dda949f3c4af93bd03c
slee <sungholee@lawn-128-61-71-60.lawn.gatech.edu>
2014-09-03 12:48:05 -0400

    sssssssss

--- gitTest1/src/gittest1/GitTest1.java
+++ gitTest1/src/gittest1/GitTest1.java
@@ -7,6 +7,7 @@
 package gittest1;
 
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -23,9 +24,6 @@ public class GitTest1 extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-        //sss
-        Scene scene2 = new Scene(root);
-        
         
         stage.setScene(scene);
         stage.show();
